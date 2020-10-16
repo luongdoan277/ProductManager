c72c6c9e4f9f9c7fbaf7d057eac98ec711c617d4
luongdoan277 <luongdoan277@gmail.com>
2020-10-16 10:22:41 +0700

    Practice

---
+++ ProductManager/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductManager.Models;
+using ProductManager.Services;
+
+namespace ProductManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
